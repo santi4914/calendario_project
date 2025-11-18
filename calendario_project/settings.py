07628ab4f7f3005495c48669dc9d3ba5552bb6e0
Santiago <santiago.aguino@correounivalle.edu.co>
2025-11-18 03:59:15 -0500

    Initial project setup:Calendar with Notes UPDATE

--- calendario_project/settings.py
+++ calendario_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'notas', # Nuestra aplicación de notas
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
